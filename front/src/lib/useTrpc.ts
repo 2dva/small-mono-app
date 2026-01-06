1dc7f03375f607daf3581a59883aac12d1267843
2dva <x7bb@mail.ru>
2026-01-07 00:04:59 +0300

    Trpc client refactor

--- front/src/lib/useTrpc.ts
+++ front/src/lib/useTrpc.ts
@@ -1,13 +1,40 @@
 import { createTRPCVueQueryClient } from '@falcondev-oss/trpc-vue-query'
+import { App, inject } from 'vue'
 import type { TrpcRouter } from '@small-mono-app/backend/src/router'
-import { inject } from 'vue'
+import { useQueryClient } from '@tanstack/vue-query'
+import { httpBatchLink, httpSubscriptionLink, splitLink } from '@trpc/client'
+import Cookies from 'js-cookie'
 
-let authToken: string | null = null
+export const createTrpcClient = (app: App) => {
+  const queryClient = app.runWithContext(useQueryClient)
+
+  const trpc = createTRPCVueQueryClient<TrpcRouter>({
+    queryClient,
+    trpc: {
+      links: [
+        splitLink({
+          // uses the httpSubscriptionLink for subscriptions
+          condition: (op) => op.type === 'subscription',
+          true: httpSubscriptionLink({
+            url: `http://localhost:3000/trpc`,
+          }),
+          false: httpBatchLink({
+            url: `http://localhost:3000/trpc`,
+            headers: () => {
+              const token = Cookies.get('token')
+              return {
+                ...(token && { authorization: `Bearer ${token}` }),
+              }
+            },
+          }),
+        }),
+      ],
+    },
+  })
+
+  return trpc
+}
 
 export function useTRPC() {
   return inject('trpc') as ReturnType<typeof createTRPCVueQueryClient<TrpcRouter>>
-}
-
-export function setAuthToken(token: string | null) {
-  authToken = token
 }
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,11 @@
 import './style.css'
-import { createTRPCVueQueryClient } from '@falcondev-oss/trpc-vue-query'
-import type { TrpcRouter } from '@small-mono-app/backend/src/router'
-import { VueQueryPlugin, useQueryClient } from '@tanstack/vue-query'
-import { httpBatchLink, httpSubscriptionLink, splitLink } from '@trpc/client'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import naive from 'naive-ui'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './lib/router'
 import { createPinia } from 'pinia'
-import Cookies from 'js-cookie'
+import { createTrpcClient } from './lib/useTrpc'
 
 const app = createApp(App)
 
@@ -18,32 +15,7 @@ app.use(naive)
 app.use(VueQueryPlugin)
 app.use({
   install(app) {
-    const queryClient = app.runWithContext(useQueryClient)
-    const trpc = createTRPCVueQueryClient<TrpcRouter>({
-      queryClient,
-      trpc: {
-        links: [
-          splitLink({
-            // uses the httpSubscriptionLink for subscriptions
-            condition: (op) => op.type === 'subscription',
-            true: httpSubscriptionLink({
-              url: `http://localhost:3000/trpc`,
-            }),
-            false: httpBatchLink({
-              url: `http://localhost:3000/trpc`,
-              headers: () => {
-                const token = Cookies.get('token')
-                return {
-                  ...(token && { authorization: `Bearer ${token}` }),
-                }
-              },
-            }),
-          }),
-        ],
-      },
-    })
-
-    app.provide('trpc', trpc)
+    app.provide('trpc', createTrpcClient(app))
   },
 })
 
