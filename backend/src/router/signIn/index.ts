f86f3d2a2e15e1424c72c11135bae11884dd1b6c
2dva <x7bb@mail.ru>
2026-01-07 11:46:14 +0300

    Add salt to pwd hash

--- backend/src/router/signIn/index.ts
+++ backend/src/router/signIn/index.ts
@@ -2,17 +2,18 @@ import crypto from 'crypto'
 import { trpc } from '../../lib/trpc'
 import { zSignInTrpcInput } from './input'
 import { signJWT } from '../../utils/signJWT'
+import { getPasswordHash } from '../../utils/getPasswordHash'
 
-export const signInTrpcRoute = trpc.procedure.input(zSignInTrpcInput).mutation(async ({ctx, input}) => {
+export const signInTrpcRoute = trpc.procedure.input(zSignInTrpcInput).mutation(async ({ ctx, input }) => {
   const user = await ctx.prisma.user.findFirst({
     where: {
       nick: input.nick,
-      password: crypto.createHash('sha256').update(input.password).digest('hex')
+      password: getPasswordHash(input.password),
     },
   })
   if (!user) throw Error('Wrong nick or pswd')
 
   const token = signJWT(user.id)
-    
+
   return { token }
 })
--- backend/src/router/signUp/index.ts
+++ backend/src/router/signUp/index.ts
@@ -2,8 +2,9 @@ import crypto from 'crypto'
 import { trpc } from '../../lib/trpc'
 import { zSignUpTrpcInput } from './input'
 import { signJWT } from '../../utils/signJWT'
+import { getPasswordHash } from '../../utils/getPasswordHash'
 
-export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ctx, input}) => {
+export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ ctx, input }) => {
   console.log(`SignUp:signUpTrpcRoute:start: nick=${input.nick}`)
   try {
     const exUser = await ctx.prisma.user.findUnique({
@@ -12,15 +13,15 @@ export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(a
       },
     })
     if (exUser) throw new Error('Already exists')
-  } catch(err) {
+  } catch (err) {
     console.log(`SignUp:signUpTrpcRoute:Error: ${err}`)
   }
 
   const user = await ctx.prisma.user.create({
     data: {
       nick: input.nick,
-      password: crypto.createHash('sha256').update(input.password).digest('hex')
-    }
+      password: getPasswordHash(input.password),
+    },
   })
   const token = signJWT(user.id)
   console.log(`SignUp:signUpTrpcRoute:end`)
