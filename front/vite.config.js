4c0a81a22cbcdf7acc7f38ca4e0ffa09fb704853
2dva <x7bb@mail.ru>
2026-01-29 21:38:38 +0300

    Minor config update

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,18 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import { parsePublicEnv } from './src/lib/parsePublicEnv'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const publicEnv = Object.entries(env).reduce((acc, [k,v]) => {
-    if (k.startsWith('VITE_') || k === 'NODE_ENV') {
-      return {
-        ...acc,
-        [k]: v,
-      }
-    }
-    return acc
-  }, {})
+  const publicEnv = parsePublicEnv(env)
 
   return {
     root: './',
